de661f4d84a30b068ae365a0c300ac2295733cf0
VP Keerthan <keerthanvp@gmail.com>
2021-06-13 12:10:15 +0530

    Initial Commit

---
+++ src/main/java/com/vpk/tutorial/jpahibernatetutorial/JpaHibernateTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.vpk.tutorial.jpahibernatetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vpk/tutorial/jpahibernatetutorial/JpaHibernateTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpk.tutorial.jpahibernatetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
